057d72f8e0728dddcae972d52b4a5707556e4a58
SouliMathieu <soulimathieu65@gmail.com>
2026-08-15 03:36:22 +0100

    Add CORS origins for production URLs

--- bse-backend/server.js
+++ bse-backend/server.js
@@ -18,6 +18,8 @@ app.use(cors({
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
+    'https://burkina-solar-energy.netlify.app',
+    'https://bse-admin.netlify.app',
   ],
   credentials: true,
 }));
